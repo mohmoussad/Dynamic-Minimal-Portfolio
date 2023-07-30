e69763ff732e117bd9b24718da96aec109cbed9e
mohmousad <moh.moussad@gmail.com>
2023-07-30 18:33:05 +0300

    Feature: Dynamic title and favicon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
+import { Helmet } from "react-helmet";
 import Profile from "./components/Profile";
 import Details from "./components/Details";
+import conifguration from "./configuration";
+
+const { name, photo } = conifguration;
 
 function App() {
   return (
-    <div>
-      <div className='layout d-flex flex-column flex-md-row '>
-        <Profile />
-        <Details />
+    <>
+      <Helmet>
+        <title>{name}</title>
+        <link rel='icon' type='image/x-icon' href={photo} />
+      </Helmet>
+      <div>
+        <div className='layout d-flex flex-column flex-md-row '>
+          <Profile />
+          <Details />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
